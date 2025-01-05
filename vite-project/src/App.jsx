114f2543f4cd46cdbe6dd263cc87aec98aec5c9c
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-05 14:18:00 +0330

    fetch & showing data

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
   const [data, setData] = useState([])
@@ -6,10 +6,39 @@ const App = () => {
   const [loading, setLoading] = useState(false)
   const [errorMsg, setErrorMsg] = useState(null)
 
-  
+  const fetchData = async () => {
+    try {
+        setLoading(true)
+        const res = await fetch('https://dummyjson.com/products?limit=100')
+        const data = await res.json()
+        
+        console.log(data)
+        if(data && data.products) {
+            setData(data.products)
+            setLoading(false)
+        }
+    }
+    catch (e) {
+       console.log(e.message)
+       setErrorMsg(e.message) 
+       setLoading(false)
+    }
+}
+
+  useEffect(() => {
+    fetchData()
+}, [])
+
+if (loading) {
+  return <div>Loading data! Please Wait.</div>}
+
+if(errorMsg) {
+  return <div>Error ! {errorMsg}</div>}
 
   return (
-    <div>App</div>
+    <div>
+      { data ? data.map((dataItem, index) => <p className='font-bold' key={index}>{dataItem.title}</p>) : null }
+    </div>
   )
 }
 
